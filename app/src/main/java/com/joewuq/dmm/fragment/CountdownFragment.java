6f71acbb94d96307192f13e47d2a3eedf92d8296
Joe Wu <joewuq@gmail.com>
2015-01-10 20:46:31 -0800

    Increased toolbar elevation when scrolling.

--- app/src/main/java/com/joewuq/dmm/fragment/CountdownFragment.java
+++ app/src/main/java/com/joewuq/dmm/fragment/CountdownFragment.java
@@ -3,6 +3,7 @@ package com.joewuq.dmm.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -31,12 +32,27 @@ public class CountdownFragment extends Fragment {
 
         recyclerView = (RecyclerView) view.findViewById(R.id.rv_list);
         recyclerView.setHasFixedSize(true);
-        // recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         cardAdapter = new CountdownCardAdapter(getActivity());
         recyclerView.setAdapter(cardAdapter);
+
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                ActionBarActivity activity = (ActionBarActivity) getActivity();
+                int elevation_resource_id = newState == RecyclerView.SCROLL_STATE_IDLE ? R.dimen.toolbar_elevation_normal : R.dimen.toolbar_elevation_lifted;
+                activity.getSupportActionBar().setElevation(getResources().getDimension(elevation_resource_id));
+            }
+        });
 
         return view;
     }
